c4fb7758fc859510de0e4206febb2bb2499bcd25
Jason Li <jligh145@gmail.com>
2025-10-22 09:55:43 -0400

    Added image update function

--- hangman.js
+++ hangman.js
@@ -1,20 +1,26 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 
-
+var guess_count = MAX_GUESSES;
 var word = "";
 var guesses = "";
+var MAX_GUESSES = 6;
 
 function newGame() {
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
 
     guesses = "";
+    guess_count = MAX_GUESSES;
     updatePage();
 }
 function guessLetter() {
-    
+
     var input = document.getElementById("guess");
     var letter = input.value;
+    //condition return -1 if letter doesn't exist in word
+    if (word.indexOf(letter) < 0) {
+        guess_count--;
+    }
     guesses += letter;
 
     updatePage();
@@ -25,16 +31,25 @@ function updatePage() {
 
     for (var i = 0; i < word.length; i++) {
         var currentLetter = word.charAt(i);
-        if(guesses.indexOf(currentLetter) >= 0){
+        if (guesses.indexOf(currentLetter) >= 0) {
             clueString += currentLetter + " ";
         } else {
             clueString += "_ ";
         }
-        
+
     }
+    //update clue string
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    //update the guesses fom thr user
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+    //update the image
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + ".gif";
+
+
+    
 }
